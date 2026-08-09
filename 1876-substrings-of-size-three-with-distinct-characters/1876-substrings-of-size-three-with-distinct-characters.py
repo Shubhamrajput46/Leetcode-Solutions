1a7764dbfd583fe230ac28253b45410dc02d439e
Shubhamrajput46 <sr9960920@gmail.com>
2026-08-09 21:12:47 +0530

    Time: 3 ms (56.25%), Space: 12.3 MB (55.31%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def countGoodSubstrings(self, s):
+        n=len(s)
+        ans =0
+        for i in range(0,n-2):
+            if s[i]!=s[i+1] and s[i+1]!=s[i+2] and s[i+2]!=s[i]:
+                ans +=1
+        return ans        
