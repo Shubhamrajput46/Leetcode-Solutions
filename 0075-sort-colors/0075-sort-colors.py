8bbb0b7ac62ecdad588746c534e15ca69856b5c7
Shubhamrajput46 <sr9960920@gmail.com>
2026-08-13 20:25:20 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.94%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def sortColors(self, nums):
+        left = 0
+        right=len(nums)-1
+        i=0
+
+        while i<=right:
+            if nums[i]==1:
+                i+=1
+            elif nums[i]==0:
+                temp=nums[i]
+                nums[i]=nums[left]
+                nums[left]=temp
+                i+=1
+                left +=1
+            else:
+                temp=nums[i]
+                nums[i]=nums[right]
+                nums[right]=temp
+                right -=1        
