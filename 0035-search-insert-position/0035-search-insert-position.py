eae204a8c05e60173eb54f88607d7f846ad6a672
Shubhamrajput46 <sr9960920@gmail.com>
2026-08-14 17:55:03 +0530

    Time: 0 ms (100%), Space: 13 MB (30.02%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def lowerbound(self, nums, target):
+        n=len(nums)
+        l=0
+        r=n-1
+        ans=n
+
+        while l<=r:
+            mid=(l+r)//2
+
+            if nums[mid]>=target:
+                ans=mid
+                r=mid-1
+            else:
+                l=mid+1
+
+        return ans
+
+    def searchInsert(self, nums, target):
+        return self.lowerbound(nums,target)
+
+        
+        
