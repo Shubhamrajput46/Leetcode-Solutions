005380466d3f8df865050be9e3b31107a584fd72
Shubhamrajput46 <sr9960920@gmail.com>
2026-08-30 23:59:43 +0530

    Time: 26 ms (67.35%), Space: 20.6 MB (38.78%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -8,13 +8,9 @@ class Solution(object):
         left = min(min_index, max_index)
         right = max(min_index, max_index)
 
-        # Case 1: Remove both from front
         front = right + 1
-
-        # Case 2: Remove both from back
         back = n - left
 
-        # Case 3: One from front, one from back
         both = (left + 1) + (n - right)
 
         return min(front, back, both)
